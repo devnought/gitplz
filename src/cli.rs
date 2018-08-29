68f9c50767380813332c006628ec0df382a01b6c
Kyle Gretchev <kgretchev@gmail.com>
2018-08-28 22:08:14 -0400

    Switch CLI arg parsing to structopt

--- src/cli.rs
+++ src/cli.rs
@@ -1,168 +1,77 @@
-use clap::{
-    crate_authors, crate_version, value_t, App, Arg, ArgGroup, ArgMatches, Shell, SubCommand,
-};
-use std::{io, path::PathBuf};
+use std::path::PathBuf;
+use structopt::{clap::ArgGroup, StructOpt};
 
 const APP_NAME: &str = "git plz";
 const CMD_BRANCH: &str = "branch";
-const CMD_CHECKOUT: &str = "checkout";
-const CMD_COMPLETIONS: &str = "completions";
-const CMD_RESET: &str = "reset";
-const CMD_STATUS: &str = "status";
-const BRANCH: &str = "branch";
-const DELETE: &str = "delete";
-const FIND: &str = "find";
-const PATH: &str = "path";
-const SHELL: &str = "shell";
 
-#[derive(Debug)]
-crate enum CommandArg {
-    Completions {
-        shell: Shell,
-    },
-    Help,
-    Run {
-        path: Option<PathBuf>,
-        option: RunOption,
-    },
+fn branch_arg_group() -> ArgGroup<'static> {
+    ArgGroup::with_name("branch").required(true)
 }
 
-#[derive(Debug)]
+#[derive(StructOpt, Debug)]
+crate struct PathArg {
+    /// Path to execute command. Defaults to working directory.
+    #[structopt(name = "path", parse(from_os_str))]
+    value: Option<PathBuf>,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(raw(bin_name = "APP_NAME"))]
 crate enum RunOption {
+    /// Perform bulk local branch operations
+    #[structopt(name = "branch", raw(group = "branch_arg_group()"))]
     Branch {
-        branch: String,
-        option: BranchOption,
+        #[structopt(
+            short = "f",
+            long = "find",
+            group = "branch",
+            raw(value_name = "CMD_BRANCH")
+        )]
+        find: Option<String>,
+        #[structopt(
+            short = "d",
+            long = "delete",
+            group = "branch",
+            raw(value_name = "CMD_BRANCH")
+        )]
+        delete: Option<String>,
+        #[structopt(flatten)]
+        path: PathArg,
     },
+    /// Checkout branch across repos
+    #[structopt(name = "checkout")]
     Checkout {
+        /// Branch name
+        #[structopt(name = "branch")]
         branch: String,
+        #[structopt(flatten)]
+        path: PathArg,
     },
-    Reset,
-    Status,
+    /// Recursive hard reset
+    #[structopt(name = "reset")]
+    Reset {
+        #[structopt(flatten)]
+        path: PathArg,
+    },
+    /// Recursive directory search version of git status
+    #[structopt(name = "status",)]
+    Status {
+        #[structopt(flatten)]
+        path: PathArg,
+    },
 }
 
-#[derive(Debug)]
-crate enum BranchOption {
-    Delete,
-    Find,
-}
-
-fn build_cli<'a, 'b>() -> App<'a, 'b> {
-    App::new("Git, please")
-        .bin_name(APP_NAME)
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about("Run commands on a set of git repositories in a folder tree")
-        .subcommand(
-            SubCommand::with_name(CMD_BRANCH)
-                .about("Perform bulk local branch operations")
-                .group(ArgGroup::with_name(CMD_BRANCH).required(true))
-                .arg(
-                    Arg::with_name(DELETE)
-                        .short("d")
-                        .long("delete")
-                        .value_name("branch")
-                        .group(CMD_BRANCH),
-                ).arg(
-                    Arg::with_name(FIND)
-                        .short("f")
-                        .long("find")
-                        .value_name("branch")
-                        .group(CMD_BRANCH),
-                ).arg(path_arg()),
-        ).subcommand(
-            SubCommand::with_name(CMD_CHECKOUT)
-                .about("Checkout branch across repos")
-                .arg(Arg::with_name(BRANCH).required(true).help("Branch name"))
-                .arg(path_arg()),
-        ).subcommand(
-            SubCommand::with_name(CMD_COMPLETIONS)
-                .about("Generates completion scripts for your shell")
-                .arg(
-                    Arg::with_name(SHELL)
-                        .required(true)
-                        .possible_values(&Shell::variants())
-                        .help("The shell to generate the script for"),
-                ),
-        ).subcommand(
-            SubCommand::with_name(CMD_RESET)
-                .about("Recursive hard reset")
-                .arg(path_arg()),
-        ).subcommand(
-            SubCommand::with_name(CMD_STATUS)
-                .about("Recursive directory search version of git status")
-                .arg(path_arg()),
-        )
-}
-
-fn path_arg<'a, 'b>() -> Arg<'a, 'b> {
-    Arg::with_name(PATH)
-        .required(false)
-        .value_name(PATH)
-        .help("Path to execute command. Defaults to working directory.")
-}
-
-fn get_path(matches: Option<&ArgMatches<'_>>) -> Option<PathBuf> {
-    if let Some(m) = matches {
-        if m.is_present(PATH) {
-            return Some(value_t!(m, PATH, String).unwrap().into());
+impl RunOption {
+    crate fn path(&self) -> Option<&std::path::Path> {
+        match self {
+            RunOption::Branch { path, .. } => path.value.as_ref().map(|x| x.as_path()),
+            RunOption::Checkout { path, .. } => path.value.as_ref().map(|x| x.as_path()),
+            RunOption::Reset { path, .. } => path.value.as_ref().map(|x| x.as_path()),
+            RunOption::Status { path, .. } => path.value.as_ref().map(|x| x.as_path()),
         }
     }
-
-    None
 }
 
-crate fn handle_args() -> CommandArg {
-    let matches = build_cli().get_matches();
-
-    match matches.subcommand() {
-        (CMD_BRANCH, Some(branch_matches)) => {
-            let option = {
-                if branch_matches.is_present(DELETE) {
-                    BranchOption::Delete
-                } else if branch_matches.is_present(FIND) {
-                    BranchOption::Find
-                } else {
-                    panic!("Invalid branch option");
-                }
-            };
-
-            CommandArg::Run {
-                path: get_path(Some(branch_matches)),
-                option: RunOption::Branch {
-                    branch: value_t!(branch_matches, CMD_BRANCH, String).unwrap(),
-                    option,
-                },
-            }
-        }
-        (CMD_CHECKOUT, Some(checkout_matches)) => CommandArg::Run {
-            path: get_path(Some(checkout_matches)),
-            option: RunOption::Checkout {
-                branch: value_t!(checkout_matches, BRANCH, String).unwrap(),
-            },
-        },
-        (CMD_COMPLETIONS, Some(shell_matches)) => CommandArg::Completions {
-            shell: value_t!(shell_matches, SHELL, Shell).unwrap(),
-        },
-        (CMD_RESET, m) => CommandArg::Run {
-            path: get_path(m),
-            option: RunOption::Reset,
-        },
-        (CMD_STATUS, m) => CommandArg::Run {
-            path: get_path(m),
-            option: RunOption::Status,
-        },
-
-        // By default, show help.
-        _ => CommandArg::Help,
-    }
-}
-
-crate fn gen_completions_for(shell: Shell) {
-    build_cli().gen_completions_to(APP_NAME, shell, &mut io::stdout())
-}
-
-crate fn print_help() {
-    build_cli()
-        .print_help()
-        .expect("Could not print command line help message");
+crate fn handle_args() -> RunOption {
+    RunOption::from_args()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![warn(rust_2018_idioms)]
 
+#[macro_use]
+extern crate structopt;
+
 mod cli;
 mod dispatcher;
 
 use color_printer::ColorPrinter;
 use command::*;
-use crate::{
-    cli::{BranchOption, CommandArg, RunOption},
-    dispatcher::Dispatcher,
-};
+use crate::{cli::RunOption, dispatcher::Dispatcher};
 use std::{
     env,
     path::PathBuf,
@@ -18,35 +18,30 @@ use threadpool::ThreadPool;
 use util::RepoIter;
 
 fn main() {
-    let (working_path, run_option) = {
-        match cli::handle_args() {
-            CommandArg::Help => {
-                cli::print_help();
-                return;
-            }
-            CommandArg::Completions { shell } => {
-                cli::gen_completions_for(shell);
-                return;
-            }
-            CommandArg::Run { path, option } => (
-                path.unwrap_or(env::current_dir().expect("Could not get working directory")),
-                option,
-            ),
-        }
-    };
+    let run_option = cli::handle_args();
+    let working_path = run_option
+        .path()
+        .map(|x| x.into())
+        .unwrap_or(env::current_dir().expect("Could not get working directory"));
 
     let is_terminal = atty::is(atty::Stream::Stdout);
     let stream = color_printer::StandardStream::stdout(color_printer::ColorChoice::Auto);
     let printer = ColorPrinter::new(is_terminal, &stream);
 
     let command: Box<dyn Command> = match run_option {
-        RunOption::Branch { branch, option } => match option {
-            BranchOption::Delete => Box::new(BranchDeleteCommand::new(branch)),
-            BranchOption::Find => Box::new(BranchFindCommand::new(branch)),
-        },
-        RunOption::Checkout { branch } => Box::new(CheckoutCommand::new(branch)),
-        RunOption::Reset => Box::new(ResetCommand::new()),
-        RunOption::Status => Box::new(StatusCommand::new()),
+        RunOption::Branch { delete, find, .. } => {
+            // TODO: This needs to be an enum again
+            if let Some(branch) = delete {
+                Box::new(BranchDeleteCommand::new(branch))
+            } else if let Some(branch) = find {
+                Box::new(BranchFindCommand::new(branch))
+            } else {
+                panic!("Invalid branch option");
+            }
+        }
+        RunOption::Checkout { branch, .. } => Box::new(CheckoutCommand::new(branch)),
+        RunOption::Reset { .. } => Box::new(ResetCommand::new()),
+        RunOption::Status { .. } => Box::new(StatusCommand::new()),
     };
 
     let pool = threadpool::Builder::new().build();
